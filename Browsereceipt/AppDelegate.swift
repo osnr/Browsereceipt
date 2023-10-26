e1206d0debb1eafb5b4982101969fe1a825648de
Omar Rizwan <omar@omar.website>
2023-10-26 16:22:31 -0400

    Initial Commit

---
+++ Browsereceipt/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Browsereceipt
+//
+//  Created by Omar Rizwan on 10/26/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Browsereceipt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Browsereceipt
+//
+//  Created by Omar Rizwan on 10/26/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
