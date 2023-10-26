edc092ceec0d61b3c1e242910f28946a4d5d6b59
Omar Rizwan <omar@omar.website>
2023-10-26 17:23:16 -0400

    Report scroll events from browser to Swift

--- Browsereceipt/ViewController.swift
+++ Browsereceipt/ViewController.swift
@@ -9,15 +9,36 @@ import Cocoa
 import WebKit
 
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, WKNavigationDelegate, WKScriptMessageHandler {
     @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         self.webView.pageZoom = 0.5;
         self.webView.load(URLRequest(url: URL(string: "https://en.m.wikipedia.org/wiki/Receipt")!))
+        self.webView.navigationDelegate = self
+    }
+    func webView(
+        _ webView: WKWebView,
+        didFinish navigation: WKNavigation!
+    ) {
+        print("Finished nav")
+        // TODO: Report entire PDF of rendered page to us
+        webView.configuration.userContentController.add(self, name:"didScroll")
+        webView.evaluateJavaScript("""
+window.addEventListener("scroll", (event) => {
+   window.webkit.messageHandlers.didScroll.postMessage(window.scrollY);
+});
+
+""")
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+       if message.name == "didScroll" {
+           print(message.body)
+       }
     }
 
     override var representedObject: Any? {
