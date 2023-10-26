298f44f6e9abca09bc54ae32e0ef9ac57d036ace
Omar Rizwan <omar@omar.website>
2023-10-26 18:44:13 -0400

    Print image of page

--- Browsereceipt/ViewController.swift
+++ Browsereceipt/ViewController.swift
@@ -12,22 +12,21 @@ import PythonKit
 class ViewController: NSViewController, WKNavigationDelegate, WKScriptMessageHandler {
     @IBOutlet weak var webView: WKWebView!
     
+    var driver: PythonObject!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set up the Python
         let sys = Python.import("sys")
         let os = Python.import("os")
-        let io = Python.import("io")
         sys.path.append("/Users/osnr/aux/Cat-Printer")
         os.chdir("/Users/osnr/aux/Cat-Printer")
         let printer = Python.import("printer")
-        let driver = printer.PrinterDriver()
+        driver = printer.PrinterDriver()
         driver.dump = true
         // MX05-F57F
         driver.connect(address: "7F4F5A11-4A6F-EB58-0089-56B126124A02")
-        
-        driver.print(io.BytesIO(PythonBytes("Hello\n\n\n\n".utf8)), mode: "text")
         
         // Set up the browser
         self.webView.pageZoom = 0.5;
@@ -40,13 +39,26 @@ class ViewController: NSViewController, WKNavigationDelegate, WKScriptMessageHan
     ) {
         print("Finished nav")
         
+        let io = Python.import("io")
+        
         // TODO: Report entire PDF of rendered page to us
-        webView.takeSnapshot(with: nil) { imOpt, err in
-            if let im = imOpt {
-                try? im.tiffRepresentation?.write(to: URL(filePath: "/Users/osnr/blup.tiff"))
-                
+        webView.takeSnapshot(with: nil) { im, err in
+            guard let tiff = im?.tiffRepresentation else { return }
+            let directory = NSTemporaryDirectory()
+            let fileName = NSUUID().uuidString
+            guard let temp = NSURL.fileURL(withPathComponents: [directory, fileName]) else { return }
+            guard ((try? tiff.write(to: temp)) != nil) else { return }
+            let task = Process()
+            let tempPbm = temp.deletingPathExtension().appendingPathExtension("pbm")
+            task.launchPath = "/opt/homebrew/bin/convert"
+            task.arguments = [temp.path(), "-resize", "384x", tempPbm.path()]
+            task.launch()
+            task.waitUntilExit()
+            print(tempPbm)
+            
+            guard let pbmData = NSData(contentsOf: tempPbm) else { return }
+            self.driver.print(io.BytesIO(PythonBytes(pbmData)), mode: "pbm")
                 // driver.print(io.BytesIO(PythonBytes(pbmData)), mode: "pbm")
-            }
         }
         
         webView.configuration.userContentController.add(self, name:"didScroll")
